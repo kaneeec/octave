03561292bf08d7a0da2f6df4264e7c4d8877572a
Tomas Zeman <tomas.zeman@oracle.com>
2017-07-13 10:50:44 +0200

    Sandbox code moved to subdir

--- sandbox.m
+++
@@ -1,9 +0,0 @@
-load("switch_data");
-
-time = switch_data(2:end, 1);
-in_deltas = diff(switch_data(:, 4:51));
-out_deltas = diff(switch_data(:, 59:106));
-
-plot(time, in_deltas(:, 5));
-
-diff([1 2 3; 2 4 7; 4 8 11])
